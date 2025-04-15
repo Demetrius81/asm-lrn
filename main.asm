be92541f4ea7e1eb0aa02b6e0f931c67cec70d09
Dzmitry Ryzhov <demetrius81ryzhov@gmail.com>
2025-04-15 18:20:15 +0300

    feat: add func lsn

--- main.asm
+++ main.asm
@@ -1,97 +1,37 @@
 .686
-.model flat, stdcall
-option casemap:none
-;===================================
-include C:\masm32\include\kernel32.inc
-includelib C:\masm32\lib\kernel32.lib
-
-
-
-.data
-	StackBottom		dd	0h
-					dd	0h
-					dd	0h
-					dd	0h
-	StackTop		dd	0h
-	StackPointer	dd	offset StackTop
-	
-	number1			dd	0h
-	number2			dd	1h
+include main.inc
 
 .code
 start:
+	call main
+	;---------
+	push eax
+	;---------
+	call ExitProcess
+;=======================================
+main proc
 
-comment~	
-	mov ebp,StackPointer
-	
+	;sub esp,8					;ccall
+	;mov dword ptr[esp+4],8		;ccall
+	;mov dword ptr[esp],7		;ccall
+	;call addNumbersCCall		;ccall
+	;add esp,8					;ccall
 	;---------
-	;Push a value in stack
+	push 8
+	push 7
+	;push FPS
 	;---------
-	
-	sub ebp,4
-	mov dword ptr[ebp],1
+	call addNumbersFC
 	;---------
-	sub ebp,4
-	mov dword ptr[ebp],2
-	;---------
-	sub ebp,4
-	mov dword ptr[ebp],3
-	;---------
-	sub ebp,4
-	mov dword ptr[ebp],4
-	;---------
-	
-	;---------
-	;Pop a value from stack
-	;---------
-	
-	mov eax,dword ptr[ebp]
-	add ebp,4
-	;---------
-	mov ecx,dword ptr[ebp]
-	add ebp,4
-	;---------
-	mov edx,dword ptr[ebp]
-	add ebp,4
-	;---------
-	mov ebx,dword ptr[ebp]
-	add ebp,4
-	;---------
-	
-	push 4
-	push 3
-	push 2
-	push 1
-	;---------
-	pop eax
-	pop ecx
-	pop edx
-	pop ebx
-	;---------
-~
-
-	push 2
-	push 1
-	;---------
-	pop number1
-	pop number2
-	;---------
-	push number2
-	push number1
-	;---------
-	pop number2
-	pop number1
-	;---------
-	
-	
-	;---------
-	push 1000
+	push FPS
 	;---------
 	call Sleep
 	;---------
 	push 0
 	;---------
-	call ExitProcess
-	;---------
+	mov eax,0
+
+	ret
+main endp
 
 end start
